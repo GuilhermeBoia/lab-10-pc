9335187c1514242ad84ec041a4437909882f7d22
Guilherme Boia <guidantasboia04@gmail.com>
2025-04-03 16:33:52 -0300

    build: commit inicial apos spring initializr

---
+++ lab-10/src/test/java/pc_bap/OnlineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pc_bap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
